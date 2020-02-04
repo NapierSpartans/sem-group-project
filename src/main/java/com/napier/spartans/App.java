d4616cb2e9edb3f4fc603fb7dedf8af52a4792c9
Nathan Nicol <40444835@live.napier.ac.uk>
2020-02-04 13:48:41 +0000

    Changed database connection string

--- src/main/java/com/napier/spartans/App.java
+++ src/main/java/com/napier/spartans/App.java
@@ -36,7 +36,7 @@ public class App {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
